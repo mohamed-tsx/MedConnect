3f93ae799b87d7bcaa4ccc94eae1b3d3721211cc
Mohamed <mohammedmohamuud.01@gmail.com>
2024-02-18 17:59:58 +0300

    Create the api route for create doctor profile

--- Server/Src/Controllers/doctorsController.js
+++ Server/Src/Controllers/doctorsController.js
@@ -1,4 +1,22 @@
-const create = (req, res) => {};
+const create = (req, res) => {
+  //Fetch the doctor information from the request body
+  const { name, avatar, specialization, description } = req.body;
+  const hospitalId = req.user.id;
+
+  //Validation
+  if (!(name && specialization && description)) {
+    res.status(403);
+    throw new Error("Please fill all the required fields");
+  }
+
+  res.status(200).json({
+    name,
+    avatar,
+    description,
+    hospitalId,
+    specialization,
+  });
+};
 
 module.exports = {
   create,
