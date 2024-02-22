3a88edf1d6ded170847bd0eaf896d2e0a58c9017
Mohamed <mohammedmohamuud.01@gmail.com>
2024-02-22 16:30:53 +0300

    Create the page and components for letting the patient see his/her appointments

--- Client/src/Pages/MyAppointments.tsx
+++ Client/src/Pages/MyAppointments.tsx
@@ -1,5 +1,11 @@
+import Appointments from "../Components/Appointments";
+
 const MyAppointments = () => {
-  return <div>MyAppointments</div>;
+  return (
+    <div>
+      <Appointments />
+    </div>
+  );
 };
 
 export default MyAppointments;
