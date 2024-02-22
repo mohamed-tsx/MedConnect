ad50b3cd5c68861f8bf3393690294915adddcc99
Mohamed <mohammedmohamuud.01@gmail.com>
2024-02-22 11:47:09 +0300

    Create api route for checking all recent appointments for the current hsihospital's doctors

--- Server/Src/Controllers/appointmentController.js
+++ Server/Src/Controllers/appointmentController.js
@@ -5,16 +5,31 @@ const AddAppointment = asyncHandler(async (req, res) => {
   const patientId = req.user.id;
   const doctorId = req.params.id;
 
+  const doctor = await Prisma.doctor.findUnique({ where: { id: doctorId } });
+
+  const hospitalId = doctor.hospitalId;
+
   const appointment = await Prisma.appointment.create({
     data: {
       patientId,
       doctorId,
+      hospitalId,
     },
   });
   res
     .status(201)
     .json({ message: "Appointment created successfully", appointment });
 });
+
+const allRecentAppointments = asyncHandler(async (req, res) => {
+  const hospitalId = req.params.id;
+  const appointments = await Prisma.appointment.findMany({
+    where: { hospitalId },
+    include: { Doctor: true, User: true },
+  });
+  res.status(200).json(appointments);
+});
 module.exports = {
   AddAppointment,
+  allRecentAppointments,
 };
--- Server/Src/Routes/appointmentRoutes.js
+++ Server/Src/Routes/appointmentRoutes.js
@@ -1,7 +1,17 @@
 const express = require("express");
-const { AddAppointment } = require("../Controllers/appointmentController");
+const {
+  AddAppointment,
+  allRecentAppointments,
+} = require("../Controllers/appointmentController");
+const { isHospital } = require("../Utils/userRoleVerifyer");
 const Verify = require("../Utils/verify");
 const router = express.Router();
 router.get("/addAppointment/:id", Verify, AddAppointment);
+router.get(
+  "/recentAppointments/:id",
+  Verify,
+  isHospital,
+  allRecentAppointments
+);
 
 module.exports = router;
